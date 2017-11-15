a79595a2bfb1f076244d120a416ce92acd720345
mparker <mparker@pivotal.io>
2017-11-15 16:50:53 +0900

    failing web spec

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,30 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+const React = require("react")
+const ReactDOM = require("react-dom")
+
+class PlayForm extends React.Component {
+    render(){
+        return <button>Hello World!</button>
+    }
+}
+
+describe("PlayForm", function () {
+    describe("when the request processes as 'INVALID'", function () {
+        it("displays 'INVALID!'", function () {
+            let domFixture = document.createElement("div")
+            document.body.appendChild(domFixture)
+
+            let alwaysInvalidRequest = {
+                play: (p1, p2, observer) => observer.invalid()
+            }
+
+            ReactDOM.render(
+                <PlayForm requests={alwaysInvalidRequest}/>,
+                domFixture
+            )
+
+            document.querySelector("button").click()
+            expect(domFixture.innerText).toContain("INVALID!")
+        })
+
     })
 })
