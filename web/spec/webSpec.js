321050dae0cffdd77e17d255edb6cf861bd6a917
mparker <mparker@pivotal.io>
2017-11-15 17:23:18 +0900

    first test passing and refactored

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -2,29 +2,77 @@ const React = require("react")
 const ReactDOM = require("react-dom")
 
 class PlayForm extends React.Component {
-    render(){
-        return <button>Hello World!</button>
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    submitHandler() {
+        this.setState({result: "INVALID!"})
+    }
+
+    render() {
+        return <div>
+            {this.state.result}
+            <button onClick={this.submitHandler.bind(this)}>PLAY</button>
+        </div>
     }
 }
 
 describe("PlayForm", function () {
     describe("when the request processes as 'INVALID'", function () {
-        it("displays 'INVALID!'", function () {
-            let domFixture = document.createElement("div")
-            document.body.appendChild(domFixture)
-
-            let alwaysInvalidRequest = {
-                play: (p1, p2, observer) => observer.invalid()
-            }
-
-            ReactDOM.render(
-                <PlayForm requests={alwaysInvalidRequest}/>,
-                domFixture
-            )
-
-            document.querySelector("button").click()
-            expect(domFixture.innerText).toContain("INVALID!")
+        beforeEach(function () {
+            renderApp({play: (p1, p2, observer) => observer.invalid()})
         })
 
+        it("displays 'INVALID!'", function () {
+            expect(page()).not.toContain("INVALID!")
+            submitForm()
+            expect(page()).toContain("INVALID!")
+        })
     })
+
+    let domFixture
+
+    function setupDOM() {
+        domFixture = document.createElement("div")
+        document.body.appendChild(domFixture)
+    }
+
+    beforeEach(function () {
+        setupDOM()
+    })
+
+    function cleanupDOM() {
+        domFixture.remove()
+    }
+
+    afterEach(function () {
+        cleanupDOM()
+    })
+
+    function renderApp(alwaysInvalidRequest) {
+        ReactDOM.render(
+            <PlayForm requests={alwaysInvalidRequest}/>,
+            domFixture
+        )
+    }
+
+    function page() {
+        return domFixture.innerText;
+    }
+
+    function submitForm() {
+        document.querySelector("button").click()
+    }
 })
+
+
+
+
+
+
+
+
+
+
